159b05372b1aa8efbeb68f0a64d83e6af5a3c8bb
Martin Funk <Martin@MartinFunk.de>
2019-11-08 17:09:06 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import logo from "./logo.svg";
 
@@ -27,6 +28,7 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/dashboard" component={Dashboard} />
           </div>
         </Router>
       </Provider>

54d7c336e253a317e85f1994862d8407b65a5b1e
Stephan Eisler <stephan@eisler.de>
2019-11-08 10:23:00 +0100

    part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import logo from "./logo.svg";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="container">
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
+
+
+
+{/* 
+          <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="https://github.com/mafulafunk/ws-todo-app" target="_blank" rel="noopener noreferrer">
               <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
@@ -35,9 +61,4 @@ class App extends Component {
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
         </div>
-      </Router>
-    );
-  }
-}
-
-export default App;
+      </Router>  */}

6a7757319ad3f9a1a242d55cb77a071be23720f8
Martin Funk <Martin@MartinFunk.de>
2019-12-10 11:41:28 +0100

    wip

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,70 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ToDo from './todos/ToDo';
 
-export default class TodosList extends Component {
-  constructor(props) {
-    super(props);
-    this.onDeleteToDo = this.onDeleteToDo.bind(this);
-    this.todoList = this.todoList.bind(this);
-    this.state = { todos: [], httpState: 0 };
-  }
+function TodosList(props) {
+  const [todos, setTodos] = useState([]);
+  const [httpState, sethttpState] = useState(0);
 
-  componentDidMount() {
+  useEffect(() => {
     axios.defaults.headers.common['Authorization'] = localStorage.jwtToken;
     axios
       .get('http://localhost:4000/todos/')
       .then(response => {
-        this.setState({ todos: response.data, httpState: response.status });
+        setTodos(response.data);
+        sethttpState(response.status);
         console.log('Status :' + response.status);
       })
       .catch(error => {
-        this.setState({ httpState: error.response.status });
+        sethttpState(error.response.status);
         console.log(error);
         console.log(error);
       });
-  }
+  });
 
-  onDeleteToDo(currentToDo) {
+  function onDeleteToDo(currentToDo) {
     axios.delete('http://localhost:4000/todos/' + currentToDo._id).then(res => {
       console.log(res.data);
-      const myToDos = [...this.state.todos];
+      const myToDos = [...todos];
       const index = myToDos.indexOf[currentToDo];
       if (index !== -1) {
         myToDos.splice(index, 1);
-        this.setState({ todos: myToDos });
+        setTodos(myToDos);
       }
     });
   }
 
-  render() {
-    return (
-      <div>
-        <h3>Todos List</h3>
-        {this.state.httpState === 401 && (
-          <div className='red-text'>401 Unauthorized</div>
-        )}
-        <table className='table table-striped' style={{ marginTop: 20 }}>
-          <thead>
-            <tr>
-              <th>Description</th>
-              <th>Responsible</th>
-              <th>Priority</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>{this.todoList()}</tbody>
-        </table>
-      </div>
-    );
-  }
-
-  todoList() {
-    const that = this;
-    return this.state.todos.map(function(currentTodo, i) {
-      return (
-        <ToDo todo={currentTodo} key={i} onDeleteToDo={that.onDeleteToDo} />
-      );
+  function todoList() {
+    return todos.map(function(currentTodo, i) {
+      return <ToDo todo={currentTodo} key={i} onDeleteToDo={onDeleteToDo} />;
     });
   }
+
+  return (
+    <div>
+      <h3>Todos List</h3>
+      {httpState === 401 && <div className='red-text'>401 Unauthorized</div>}
+      <table className='table table-striped' style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Responsible</th>
+            <th>Priority</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{todoList()}</tbody>
+      </table>
+    </div>
+  );
 }
+
+export default TodosList;
